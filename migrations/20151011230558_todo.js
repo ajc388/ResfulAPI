9d60579eeb1b303e47786ad514ca8e61dd1319be
Z3R0_H0 <ajc388@nau.edu>
2015-10-11 23:29:41 -0500

    1.)Attempting to implement migrations. 2.)Had to global install knex. 3.) Connection file had to be renamed knexfile.js and was moved to root directory 4.) migrations folder was scaffolded 5.)Migration has some content to add the database and drop it. TODO: test migration

---
+++ migrations/20151011230558_todo.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+	return Promise.all([
+		knex.schema.createTable('todo', function(table) {
+	        table.increments('id').primary();
+	        table.string('text');
+	        table.boolean('complete');
+       	})
+	])
+};
+
+exports.down = function(knex, Promise) {
+	return Promise.all([
+		knex.schema.dropTable('todo');
+	])
+};
